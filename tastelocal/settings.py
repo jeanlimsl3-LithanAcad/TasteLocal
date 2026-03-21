b355fb8c2a8d3c8970aac348062212fbb1441e47
Jean Lim <jeanlimsl3@gmail.com>
2026-03-22 06:51:50 +0800

    final fix - sqlite working

--- tastelocal/settings.py
+++ tastelocal/settings.py
@@ -33,15 +33,11 @@ else:
     DEBUG = True
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'tastelocal_db',
-            'USER': 'root',
-            'PASSWORD': 'NewStrongPassword123!',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
     
     
 # Application definition
