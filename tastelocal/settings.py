99ee7960dee2ce4ebd60047cd187448ea4046e4b
Jean Lim <jeanlimsl3@gmail.com>
2026-03-22 07:41:17 +0800

    add media settings

--- tastelocal/settings.py
+++ tastelocal/settings.py
@@ -97,6 +97,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+# --- MEDIA FILES ---
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # --- LOGIN ---
 LOGIN_URL = '/login/'
 
